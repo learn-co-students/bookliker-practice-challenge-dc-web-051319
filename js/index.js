7d7272166ddf2c48cf7eb119d376b30a563aa633
newtypeV2 <gianlibunao@gmail.com>
2019-07-07 15:13:19 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const BOOKSURL = "http://localhost:3000/books/"
+const USERSURL = "http://localhost:3000/users/"
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM Content Finished Loading.");
+    console.log("Connected");
+    Book.getAllBooks();
+
+});
+
+function getUlList(){
+    return document.getElementById("list");
+}
+
+function getShowPanel(){
+    return document.getElementById("show-panel");
+}
